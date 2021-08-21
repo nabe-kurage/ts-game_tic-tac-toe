cbd68f63cb4f5e765cf7c98435e69549580af76b
nabe-kurage <tabiflag@gmail.com>
2021-08-21 13:25:00 +0900

    set next player

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // change player
+    var xIsNext = true;
+    var nextPlayerView = document.getElementById('nextPlayer');
+    var changePlayer = function () {
+        xIsNext = !xIsNext;
+        if (xIsNext) {
+            nextPlayerView.innerHTML = 'x';
+        }
+        else {
+            nextPlayerView.innerHTML = 'o';
+        }
+    };
+    // click event handlar
     var square = document.getElementsByClassName('square');
     var squareArray = Array.from(square);
     squareArray.forEach(function (target) {
         target.addEventListener('click', function () {
             console.log('test');
+            changePlayer();
         });
     });
 }, false);
--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // change player
+    let xIsNext: boolean = true
+    const nextPlayerView = document.getElementById('nextPlayer')
+    const changePlayer = () => {
+        xIsNext = !xIsNext
+        if (xIsNext) {
+            nextPlayerView.innerHTML = 'x'
+        } else {
+            nextPlayerView.innerHTML = 'o'
+        }
+    }
+
+    // click event handlar
     const square = document.getElementsByClassName('square')
     const squareArray = Array.from(square)
 
     squareArray.forEach((target) => {
         target.addEventListener('click', () => {
             console.log('test')
+            changePlayer()
         })
     })
+    
 }, false);
