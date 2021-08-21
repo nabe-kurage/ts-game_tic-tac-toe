34dfa4369bc6d70e439b7e1d8a17b0c0e28ac771
nabe-kurage <tabiflag@gmail.com>
2021-08-21 14:21:22 +0900

    set mark in square

--- main.ts
+++ main.ts
@@ -1,28 +1,38 @@
 document.addEventListener(
-  'DOMContentLoaded',
-  function () {
-    // change player
-    let xIsNext: boolean = true
-    const nextPlayerView = document.getElementById('nextPlayer')
-    const changePlayer = () => {
-      xIsNext = !xIsNext
-      if (xIsNext) {
-        nextPlayerView.innerHTML = 'x'
-      } else {
-        nextPlayerView.innerHTML = 'o'
-      }
-    }
+    'DOMContentLoaded',
+    function () {
+        // change player
+        let xIsNext: boolean = true
+        const nextPlayerView: HTMLElement =
+            document.getElementById('nextPlayer')
 
-    // click event handlar
-    const square = document.getElementsByClassName('square')
-    const squareArray = Array.from(square)
+        const changePlayer = () => {
+            xIsNext = !xIsNext
+            if (xIsNext) {
+                nextPlayerView.innerHTML = 'x'
+            } else {
+                nextPlayerView.innerHTML = 'o'
+            }
+        }
 
-    squareArray.forEach((target) => {
-      target.addEventListener('click', () => {
-        console.log('test')
-        changePlayer()
-      })
-    })
-  },
-  false
+        const setMark = (target: HTMLElement): void => {
+            console.log(target)
+            if (xIsNext) {
+                target.innerHTML = 'x'
+            } else {
+                target.innerHTML = 'o'
+            }
+        }
+
+        // click event handlar
+        const square = document.getElementsByClassName('square')
+        const squareArray = Array.from(square)
+        squareArray.forEach((target) => {
+            target.addEventListener('click', () => {
+                setMark(target)
+                changePlayer()
+            })
+        })
+    },
+    false
 )
